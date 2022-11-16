f2ac9ff652de6f2788b678b74f73bd59e44eb48b
Paulo Neto <nspp@outlook.com>
2022-11-16 11:44:36 -0400

    Momento que comecei a trabalhar no menu

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function cliqueMenu () {
+    if (menuPrincipal.style.display == 'block') {
+        menuPrincipal.style.display = 'none'
+    } else {
+        menuPrincipal.style.display = 'block'
+    }
+}
